ad6aa05e17cd29b30296210487e71567e69cf641
Muhammad Sandy <mhmmdsandy@gmail.com>
2023-05-13 11:13:14 +0700

    pertemuan 7

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+        
+        if($validator->fails())
+        {
+            $messages = $validator->messages();
+            return response()->json(['message' => 'error', 'data' => $messages ], 400);            
+        }
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'api_token' => Str::random(60)
+        ]);
+
+        return response()->json(['message' => 'berhasil create', 'data' => $user], 200);
+    }
+}
